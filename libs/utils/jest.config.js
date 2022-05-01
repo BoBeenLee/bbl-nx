0581b02bff25808a2a99de3ccb44636828916aba
bobinlee <bobeen.lee@huiseoul.com>
2022-05-01 14:42:50 +0900

    refactor: rename util -> utils

--- libs/utils/jest.config.js
+++ libs/utils/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'util',
+  displayName: 'utils',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/util',
+  coverageDirectory: '../../coverage/libs/utils',
 };
