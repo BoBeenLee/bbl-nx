905855b6e1f5e9aaa3654831ad6b3e801ce62756
bobinlee <bobeen.lee@huiseoul.com>
2022-05-07 18:50:07 +0900

    refactor: move to org lib

--- libs/organizations/src/index.ts
+++ libs/organizations/src/index.ts
@@ -1,1 +1,8 @@
 export * from './lib/organizations';
+export * from './lib/about/activity/activity';
+export * from './lib/about/education/education';
+export * from './lib/about/experience/experience';
+export * from './lib/about/skill/skill';
+export * from './lib/portfolio/portfolio';
+export * from './lib/rotate/rotate';
+
