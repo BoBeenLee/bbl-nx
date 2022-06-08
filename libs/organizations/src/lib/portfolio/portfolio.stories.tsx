39d1b77199ee9d807f07394c5b928f9accc5e7c5
bobinlee <bobeen.lee@huiseoul.com>
2022-06-08 13:54:43 +0900

    fix: fixed title

--- libs/organizations/src/lib/portfolio/portfolio.stories.tsx
+++ libs/organizations/src/lib/portfolio/portfolio.stories.tsx
@@ -11,11 +11,11 @@ const Template: Story<PortfolioProps> = (args) => <Portfolio {...args} />;
 
 export const Company = Template.bind({});
 Company.args = {
-  title: 'Organizations/Company',
+  title: 'Company',
   portfolios: companyPortfolios,
 };
 export const Team = Template.bind({});
 Team.args = {
-  title: 'Organizations/Project',
+  title: 'Project',
   portfolios: teamPortfolios,
 };
