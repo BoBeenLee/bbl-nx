e520a9f1bead5fe899e5af486a7c6f993a6347c2
bobinlee <bobeen.lee@huiseoul.com>
2022-06-14 12:40:48 +0900

    fix: export 경로 추가

--- libs/ui-components/src/lib/card/post-card/post-card.tsx
+++ libs/ui-components/src/lib/card/post-card/post-card.tsx
@@ -1,10 +1,10 @@
 import styled, { css } from 'styled-components';
 import { GoBook as GoBookIcon } from 'react-icons/go';
 import { media, toDateTimeText, today } from '@bbl-nx/utils';
+import { DATE_TIME_FORMAT5 } from "@bbl-nx/utils";
 import { theme } from '@bbl-nx/styles';
 import Seperator from '../../seperator/seperator';
 import SubTitle from '../../title/sub-title/sub-title';
-import { DATE_TIME_FORMAT5 } from "libs/utils/src/lib/datetime/datetime-format";
 
 export interface PostCardProps {
   title: string;
