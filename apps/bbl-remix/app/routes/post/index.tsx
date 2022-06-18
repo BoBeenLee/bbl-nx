13043a03ef3a7a12f19f9902b3c5d8c82624636a
bobinlee <bobeen.lee@huiseoul.com>
2022-06-18 15:02:50 +0900

    fix: remix도 동일하게 post 조회

--- apps/bbl-remix/app/routes/post/index.tsx
+++ apps/bbl-remix/app/routes/post/index.tsx
@@ -3,56 +3,59 @@ import { Link, useLoaderData } from '@remix-run/react';
 import styled from 'styled-components';
 
    
-import { deferred } from "@remix-run/node";
-
 import { getAllPosts, PostItem as MDPostItem } from '~/libs/post.server';
 import { getFeednamiTistories, TistoryItem } from '~/libs/tistory';
 import { PostCard } from '@bbl-nx/ui-components';
-import { postMachine, PostItem } from '@bbl-nx/machines';
-import { interpret, assign, Subscription } from 'xstate';
+import { postMachine } from '@bbl-nx/machines';
+import { interpret, assign } from 'xstate';
+import { waitFor } from 'xstate/lib/waitFor';
+import { useMachine } from '@xstate/react';
+import { useEffect } from "react";
 
 const Root = styled.div`
   padding-top: 20px;
 `;
 
 
-const postService = interpret(
-  postMachine.withConfig({
-    actions: {
-      'updatePostsContext': assign((ctx, event) => {
-        return {
-          posts: [...ctx.posts, ...event.data],
-        };
-      }),
-    },
-    services: {
-      'fetchTistories': async (ctx, event) => {
-        const response = await getFeednamiTistories(
-          'http://cultist-tp.tistory.com/rss'
-        );
-        const data = mapTistoryToPosts(response);
-        return data;
-      },
-      'fetchMD': async () => {
-        const response = await getAllPosts();
-        const data = mapRemarkToPosts(response);
-        return data;
-      },
-    },
-  })
-);
+const Loading = styled.div`
+  height: 100vh;
+`;
 
-const getPostItems = () => {
-  let postServiceSubscription: Subscription | null = null;
-  return new Promise((resolve) => {
-    postService.start();
-    postServiceSubscription = postService.subscribe((state) => {
-      if (state.matches('Done')) {
-        resolve(state.context.posts);
-        postServiceSubscription?.unsubscribe?.();
-      }
-    });
-  });
+const postServiceWithConfig = postMachine.withConfig({
+  actions: {
+    updatePostsContext: assign((ctx, event) => {
+      return {
+        posts: [...ctx.posts, ...event.data],
+      };
+    }),
+  },
+  services: {
+    fetchTistories: async (__, ___) => {
+      const response = await getFeednamiTistories(
+        'http://cultist-tp.tistory.com/rss'
+      );
+      const data = mapTistoryToPosts(response);
+      return data;
+    },
+    fetchMD: async () => {
+      const response = await getAllPosts();
+      const data = mapRemarkToPosts(response);
+      return data;
+    },
+  },
+});
+
+const makeMDPostState = async () => {
+  const postService = interpret(postServiceWithConfig);
+  postService.start();
+  const doneState = await waitFor(
+    postService,
+    (state) => state.matches('FetchingPosts.FetchingMD.Done'),
+    {
+      timeout: 10_000,
+    }
+  );
+  return doneState;
 };
 
 const mapRemarkToPosts = (allMarkdownRemark: MDPostItem[]) => {
@@ -84,14 +87,27 @@ const mapTistoryToPosts = (tistories: TistoryItem[]) => {
 };
 
 export const loader = async () => {
-  const data = await getPostItems();
-  return data;
+  const state = await makeMDPostState();
+  return JSON.stringify(state);
 };
 
 export default function Posts() {
-  const posts = useLoaderData<PostItem[]>();
+  const postMachineState = useLoaderData<string>();
+  const [state, send] = useMachine(postServiceWithConfig, {
+    state: JSON.parse(postMachineState),
+  });
+
+  useEffect(() => {
+    send('FETCH');
+  }, [send]);
+
+  if (!state.matches('Done')) {
+    return <Loading>{'Loading...'}</Loading>;
+  }
+  const posts = state.context.posts;
   const postsByDESC = _.orderBy(posts, ['date'], ['desc']);
   const filterPublished = postsByDESC.filter((item) => item.published);
+
   return (
     <Root>
       {_.map(filterPublished, (item) => {
