4af6dc4bc5ee56d707ac67304ca9ffea46f94205
bobeenlee <globaldev@naver.com>
2022-06-29 13:47:49 +0900

    chore: remove unused comp

--- libs/ui-components/src/lib/molecules/index.ts
+++ libs/ui-components/src/lib/molecules/index.ts
@@ -4,7 +4,6 @@ export * from './photo-gallery/photo-gallery';
 export * from './link/social-links/social-links';
 export * from './nav/nav';
 export * from './nav-footer/nav-footer';
-export * from './home/home';
 export * from './home-card/home-card';
 export * from './experience-card/experience-card';
 export * from './activity-card/activity-card';
