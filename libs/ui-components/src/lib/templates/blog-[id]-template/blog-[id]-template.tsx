a3667d2043d8ffd7718523a01cd961d07fc911f4
bobeenlee <globaldev@naver.com>
2022-07-01 04:31:44 +0900

    feat: add blog [id] created, profile

--- libs/ui-components/src/lib/templates/blog-[id]-template/blog-[id]-template.tsx
+++ libs/ui-components/src/lib/templates/blog-[id]-template/blog-[id]-template.tsx
@@ -1,5 +1,5 @@
-import { PageTitle } from '../../atoms';
-
+import { Avatar, PageTitle } from '../../atoms';
+import { DATE_TIME_FORMAT5, toDateTimeText } from '@bbl-nx/utils';
 export interface BlogIdTemplateProps {
   id: string;
   title: string;
@@ -8,11 +8,22 @@ export interface BlogIdTemplateProps {
 }
 
 export function BlogIdTemplate(props: BlogIdTemplateProps) {
-  const { title, content } = props;
+  const { title, content, createdAt } = props;
 
   return (
     <div className="flex flex-col items-start w-full max-w-2xl mx-auto mb-8 border-gray-200 sm:mb-12 dark:border-gray-700">
       <PageTitle>{title}</PageTitle>
+      <div className="flex flex-row items-center justify-between w-full mt-2">
+        <div className="flex flex-row items-center">
+          <Avatar width={'24'} />
+          <p className="ml-2 text-sm text-gray-600 dark:text-gray-400">
+            {'BoBeen Lee'}
+          </p>
+        </div>
+        <p className="text-sm text-gray-600 dark:text-gray-400">
+          {toDateTimeText(createdAt, DATE_TIME_FORMAT5)}
+        </p>
+      </div>
       <article
         className="w-full mt-4 prose dark:prose-dark max-w-none"
         dangerouslySetInnerHTML={{ __html: content }}
