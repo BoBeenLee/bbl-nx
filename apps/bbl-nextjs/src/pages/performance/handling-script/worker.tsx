251b6fb2e767034900978594cccd19c756c7cd59
bobeenlee <globaldev@naver.com>
2022-07-02 16:16:22 +0900

    feat: add script strategy test pages

---
+++ apps/bbl-nextjs/src/pages/performance/handling-script/worker.tsx
@@ -0,0 +1,15 @@
+import Script from 'next/script';
+
+import React from 'react';
+
+export default function HandlingScriptPage() {
+  return (
+    <>
+      <Script
+        strategy={'worker'}
+        src="https://javascript.info/article/script-async-defer/long.js?speed=1"
+      />
+      <input className="text-gray-900" placeholder="input" />
+    </>
+  );
+}
