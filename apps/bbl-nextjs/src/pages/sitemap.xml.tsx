ce8de52135c19e4965211adb3234714e98491995
bobeenlee <globaldev@naver.com>
2022-07-02 21:43:16 +0900

    feat: 사이트맵 추가

---
+++ apps/bbl-nextjs/src/pages/sitemap.xml.tsx
@@ -0,0 +1,39 @@
+import { fetchMD } from "../apis/post";
+import { GetServerSideProps } from 'next';
+
+const createSitemap = (
+  slugs: string[]
+) => `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${slugs
+          .map((slug) => {
+            return `
+                <url>
+                    <loc>${`https://bbl-nx.vercel.app/${slug}`}</loc>
+                </url>
+            `;
+          })
+          .join('')}
+    </urlset>
+`;
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const allPosts = await fetchMD();
+  const allPages = [...allPosts.map(item => `blog/${item.id}`), ...['', 'about', 'blog', 'project']];
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=1200, stale-while-revalidate=600'
+  );
+  res.write(createSitemap(allPages));
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default function Sitemap() {
+  return null;
+}
