608170c44c94ca6810917a39d09db1925a3cf045
bobeenlee <globaldev@naver.com>
2022-07-03 23:18:33 +0900

    feat: update meta data

--- apps/bbl-nextjs/src/components/layout.tsx
+++ apps/bbl-nextjs/src/components/layout.tsx
@@ -26,20 +26,16 @@ export default function Layout(props: LayoutProps) {
     <>
       <Head>
         <title>{title}</title>
-        {/* <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
-        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta name="robots" content="follow, index" />
+        <meta content={metadata.description} name="description" />
+        {/* 
+        <meta property="og:url" content={`https://bbl-nx.vercel.app${router.asPath}`} />
+        <link rel="canonical" href={`https://bbl-nx.vercel.app${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Lee Robinson" />
+        <meta property="og:site_name" content="BoBeen Lee" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@leeerob" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
         {meta.date && (
           <meta property="article:published_time" content={meta.date} />
         )} */}
