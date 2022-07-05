4fe7ee3a5c51bb533f0959db3b9c048dfe2cacd0
bobeenlee <globaldev@naver.com>
2022-07-06 05:18:58 +0900

    feat: add menu list component

---
+++ libs/ui-components/src/lib/organisms/menu-list/menu-list.stories.tsx
@@ -0,0 +1,25 @@
+import { Story, Meta } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import { MenuList, MenuListProps } from './menu-list';
+import { nav } from '@bbl-nx/constants';
+
+export default {
+  component: MenuList,
+  title: 'organisms/MenuList',
+  argTypes: {},
+} as Meta;
+
+const Template: Story<MenuListProps> = (args) => <MenuList {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  data: nav.map((item) => ({ text: item.text, href: item.href, isActive: false })),
+  onNavigate: () => {
+    // SOMETHING
+  },
+};
+
+Primary.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // TODO SOMETHING
+};
