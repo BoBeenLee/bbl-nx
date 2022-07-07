9d1f342b248c06f629d99532b2dcb6684b167d21
bobeenlee <globaldev@naver.com>
2022-07-07 13:04:05 +0900

    feat: add blog search feature

--- libs/ui-components/src/lib/templates/blog-template/blog-template.tsx
+++ libs/ui-components/src/lib/templates/blog-template/blog-template.tsx
@@ -1,6 +1,8 @@
 import { PageTitle } from '../../atoms';
 import BlogCard from '../../molecules/blog-card/blog-card';
 import { PostItem } from '@bbl-nx/interfaces';
+import { useCallback, useState, useTransition } from 'react';
+import { SearchInput } from '../../molecules';
 
 export interface BlogTemplateProps {
   allPosts: PostItem[];
@@ -8,12 +10,25 @@ export interface BlogTemplateProps {
 
 export function BlogTemplate(props: BlogTemplateProps) {
   const { allPosts } = props;
+  const [filteredPosts, setFilteredPosts] = useState(allPosts);
+  const [, startTransition] = useTransition();
+
+  const onSearch = useCallback(
+    (text: string) => {
+      const searchPosts = allPosts.filter((post) => post.title.includes(text));
+      startTransition(() => {
+        setFilteredPosts(searchPosts);
+      });
+    },
+    [allPosts]
+  );
 
   return (
     <div className="flex flex-col items-start w-full max-w-2xl mx-auto mb-8 border-gray-200 sm:mb-12 dark:border-gray-700">
-      <PageTitle className="pb-6">All Posts</PageTitle>
+      <PageTitle className="pb-4">All Blogs</PageTitle>
+      <SearchInput className="w-full mb-12" onSearch={onSearch} />
       <div className="w-full space-y-12">
-        {allPosts.map((item) => {
+        {filteredPosts.map((item) => {
           const { id, title, createdAt, url, isExternal } = item;
           return (
             <div key={id}>
