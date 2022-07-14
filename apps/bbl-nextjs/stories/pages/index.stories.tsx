ce58afca707ed391b87c2fa49b4ff312f16e85be
bobeenlee <globaldev@naver.com>
2022-07-14 17:14:07 +0900

    feat: add index, project stories

---
+++ apps/bbl-nextjs/stories/pages/index.stories.tsx
@@ -0,0 +1,28 @@
+import { Story, Meta } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import IndexPage from '../../src/pages/index';
+
+export default {
+  component: IndexPage,
+  title: 'pages/Index',
+  argTypes: {},
+} as Meta;
+
+const Template: Story = (args) => <IndexPage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  asPath: '/index',
+};
+
+Primary.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // TODO SOMETHING
+};
+
+Primary.parameters = {
+  nextRouter: {
+    path: '/index',
+    asPath: '/index',
+  },
+};
---
+++ apps/bbl-nextjs/stories/pages/project.stories.tsx
@@ -0,0 +1,28 @@
+import { Story, Meta } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import ProjectPage from '../../src/pages/project/index';
+
+export default {
+  component: ProjectPage,
+  title: 'pages/Project',
+  argTypes: {},
+} as Meta;
+
+const Template: Story = (args) => <ProjectPage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  asPath: '/project',
+};
+
+Primary.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // TODO SOMETHING
+};
+
+Primary.parameters = {
+  nextRouter: {
+    path: '/project',
+    asPath: '/project',
+  },
+};
