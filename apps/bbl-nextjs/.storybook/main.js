34ac1226fbf0bcb37e5b71302acee2316d6807c4
bobeenlee <globaldev@naver.com>
2022-07-17 21:34:58 +0900

    chore: add nodejs fallback storybook config

--- apps/bbl-nextjs/.storybook/main.js
+++ apps/bbl-nextjs/.storybook/main.js
@@ -31,7 +31,32 @@ module.exports = {
     }
 
     // add your own webpack tweaks if needed
-
+    config.resolve.fallback = {
+      fs: false,
+      assert: false,
+      buffer: false,
+      console: false,
+      constants: false,
+      crypto: false,
+      domain: false,
+      events: false,
+      http: false,
+      https: false,
+      os: false,
+      path: false,
+      punycode: false,
+      process: false,
+      querystring: false,
+      stream: false,
+      string_decoder: false,
+      sys: false,
+      timers: false,
+      tty: false,
+      url: false,
+      util: false,
+      vm: false,
+      zlib: false,
+    };
     return config;
   },
   staticDirs: ['../public'],
