1574ff7f30a1d5a321bd303ace6dc2e52a0adafc
bobeenlee <globaldev@naver.com>
2022-07-18 12:43:05 +0900

    feat: remove machines libs

--- apps/bbl-nextjs/src/machines/blog-service-machine.ts
+++ apps/bbl-nextjs/src/machines/blog-service-machine.ts
@@ -1,4 +1,4 @@
-import { blogMachine } from '@bbl-nx/machines';
+import { blogMachine } from '@bbl-nx/features';
 import { fetchMD, fetchTistories } from '../apis/blog';
 
 export const blogServiceWithConfig = blogMachine.withConfig({
