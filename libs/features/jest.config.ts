43f183e677122290c13e7f5033d9e0afd7ecc28a
bobeenlee <globaldev@naver.com>
2022-07-18 12:59:04 +0900

    fix: fixed failed tests

--- libs/features/jest.config.ts
+++ libs/features/jest.config.ts
@@ -1,10 +1,21 @@
+import rootMain from '../../jest.config.base';
+
 /* eslint-disable */
 export default {
   displayName: 'features',
   preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    ...rootMain.moduleNameMapper,
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/features',
+  setupFiles: ['../../jest.setupTests.base.js'],
 };
