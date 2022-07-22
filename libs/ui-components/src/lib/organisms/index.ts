83398c12f0cda39d6a5d6bc1c872cd068fb24baa
bobeenlee <globaldev@naver.com>
2022-07-22 15:19:25 +0900

    refac: rename list to s

--- libs/ui-components/src/lib/organisms/index.ts
+++ libs/ui-components/src/lib/organisms/index.ts
@@ -1,5 +1,5 @@
 export * from './home-card/home-card';
-export * from './menu-list/menu-list';
+export * from './menus/menus';
 export * from './mobile-nav/mobile-nav';
 export * from './nav/nav';
 export * from './nav-footer/nav-footer';
