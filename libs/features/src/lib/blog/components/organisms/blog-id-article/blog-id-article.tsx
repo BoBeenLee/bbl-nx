31e063ec057a891d9c749e72d2a71bcec2bb491f
bobeenlee <globaldev@naver.com>
2022-07-22 14:58:09 +0900

    refac: abstract blog id article comp

---
+++ libs/features/src/lib/blog/components/organisms/blog-id-article/blog-id-article.tsx
@@ -0,0 +1,36 @@
+import { Avatar, SectionTitle } from '@bbl-nx/ui-components';
+import { DATE_TIME_FORMAT5, toDateTimeText } from '@bbl-nx/utils';
+
+export interface BlogIdArticleProps {
+  className?: string;
+  title: string;
+  createdAt: string;
+  content: string;
+}
+
+export function BlogIdArticle(props: BlogIdArticleProps) {
+  const { className, title, content, createdAt } = props;
+
+  return (
+    <div className={className}>
+      <SectionTitle className="pb-2">{title}</SectionTitle>
+      <div className="flex flex-row items-center justify-between w-full mt-2">
+        <div className="flex flex-row items-center">
+          <Avatar width={'24'} />
+          <p className="ml-2 text-sm text-gray-600 dark:text-gray-400">
+            {'BoBeen Lee'}
+          </p>
+        </div>
+        <p className="text-sm text-gray-600 dark:text-gray-400">
+          {toDateTimeText(createdAt, DATE_TIME_FORMAT5)}
+        </p>
+      </div>
+      <article
+        className="w-full mt-4 prose dark:prose-dark max-w-none"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </div>
+  );
+}
+
+export default BlogIdArticle;
