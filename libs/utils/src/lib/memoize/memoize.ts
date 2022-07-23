cfbfd32f36199aa6142fd2b91106fa294888ec5b
bobeenlee <globaldev@naver.com>
2022-07-23 14:41:15 +0900

    refac: update lodash to lodsah.isequal libs

--- libs/utils/src/lib/memoize/memoize.ts
+++ libs/utils/src/lib/memoize/memoize.ts
@@ -1,5 +1,5 @@
 import memoizeOne from 'memoize-one';
-import { isEqual } from 'lodash';
+import isEqual from 'lodash.isequal';
 import { identity } from '../common/common';
 
 export const deepMemoized = memoizeOne(identity, isEqual) as <T>(x: T) => T;
