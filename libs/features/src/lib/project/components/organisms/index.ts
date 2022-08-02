b8ac5ea674d553e0236145eb53a19fa8c7cd288a
bobeenlee <globaldev@naver.com>
2022-08-02 15:30:49 +0900

    feat: 내부 컴포넌트 barrel files에서 제외

--- libs/features/src/lib/project/components/organisms/index.ts
+++ libs/features/src/lib/project/components/organisms/index.ts
@@ -1,4 +1,3 @@
-export * from './project-card/project-card';
 export * from './company-portfolios/company-portfolios';
 export * from './personal-portfolios/personal-portfolios';
 export * from './team-portfolios/team-portfolios';
