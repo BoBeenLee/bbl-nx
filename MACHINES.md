cf5a77f1387a4a0dfb230569b41ad8359b14154d
bobeenlee <globaldev@naver.com>
2022-08-08 20:46:59 +0900

    chore: update readme

---
+++ MACHINES.md
@@ -0,0 +1,5 @@
+# Machines
+
+## Blog Fetching Flow
+
+- https://stately.ai/viz/21cde854-a76f-45d8-924e-60359237bbad
--- libs/features/src/lib/blog/machines/blog-machine.ts
+++ libs/features/src/lib/blog/machines/blog-machine.ts
@@ -2,98 +2,98 @@ import { createMachine, assign } from 'xstate';
 import { BlogItem } from '../interfaces/blog';
 
 export const blogMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QAcD2sAuBZAhgYwAsBLAOzADoAxMDQ0qABXQ1iprpKgBUjNUAnInDa1inAMQRUZcqQBuqANYU0mXBwrVR9JplZaO3XhgFD97MVATzUeHBiLSA2gAYAuohToiD6Z5AAHogAzKHkACwAbMEA7ABMABxxwYkAnMEAjAA0IACeiHHhCeSpAKzRMaXBpS5RMS4JAL6NOarY+GKaFjrM5tqcPHyCwgaW4mD8-ALkyAA29gBmAgC2M8zqnSKGuixbloMmw32G1iQKdr4krh5IIKo+jiT+QQihwREViclpmTn5r7EItUXKk4jFIqkYhlUk0Wnd1h1SF1+oxent6FgACLoiRSGQ2ZRrNSImSjHp6HFQLGU07neyPa7+e6XZ4hFwucgZFyhOJxDLBCHhYLhP6IGqpCIuSJC6LShLS-nNVoIjSUnbHSzUskSCZTfgzeYYJb8VZtDZItVo7VU7HW2m2enOdxM7ws24vYLsznclJ8gWpIUivJiyHkBIJUqFIWQjKRUoxJXw4mq63qyTSCiYewqFWbVO9F2wB5+d2IKExEoZQrpCOg0pC0WvGIVoWlDLhVIZNsZeKxZpwkioCBwQvtFPdTjqykHUzCTEZwvFp6lhBVDLkXkR9m1cKlBKpSKNuKROIRGpctfhLmQ0qJs0k5HbK0ToxDMzkACikwEi7doBea4bokNTsuEu77oewYAuurZtgG0qlJGCZwve44olO1ozkclK-o8rIIFycRHlU5Anj6vI9vyDSwsqyZ5i+GEvlhZi4SW-5lgkjY9qUJT1J2ELspk7Z3rmFr5hS1pai+rHLuxBEuERUFtuE5AuPUQoNNCsbBCJdFiQxz4otS85kDJ+EdqedblIKkQZFWYGNpGlnsu2tmgmCfYoaJpIGRJL7Ul+epmSuFklHEiGRDZdlRo5nZhhGcoBny4TJLpY70ehhmGFiwVyTEnFQTKERxCCzaduE9QJEKaXmj5mV6LlgRikG-yESUEbgbuLhdvGNUPuQJlgI1LwJJkG4VakkLlil2RQbEMHVPGe5ROyVR9Row1so2nr9o0QA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QCMA2B7KBZAhgYwAsBLAOzADoAxMAF0NKgCEMpYrb6SoAVI2G9ACcicdnWJcAxBHRlypAG7oA1hTSZcnCtXENmmNjs48+A4aKMSoCRejw4aRWQG0ADAF1EoAA7pYRR1kvEAAPRABmcIB2cgBOVyiARldwgDYo8IBWTKiAGhAAT0QAJgAWTLjE4qj4gA5i8NcqhIBfFvz1bHwJbQ4rfVYxY15+IRFDPoZJMEFBIXJvVAcAMyEAW3JOzR6h-pYJ3S4Rs3HdhhsSJXtAkjdPJBBffxvgsIRImPiklPSsnPyiu9wqVyNFMsVUq5SolEjVaplSm0OixtqReocmPszlwsAARbFQaSyCi2VSbFHdNEEgYHYx4gkXK4OJy3DzBJ4BFmvCIpcjFTKuVzFCGJSKpWKxAGIAUVcKJTK1BK1VKlWoS2JRJEgLaUuSWPRY-U4-FGwkzOaCBZLGirQQbHVaamGybGhm2a4su7svycoIPN6NcJ8gVCkViiVShCZWJBqJRer81Jy8KKzJah07U00olyfgONQUx1Z-be55c-2IKLVcgwmOQ+LhWLZSWFCJREG1dvRJOZeX8uNtdogEjoCBwYIZqnFgwE45jUS44ml30kblRxvkVIq2INeqxKqZSP82LkUquVICxKlSHpYqxdOFzMuzEz01z8xsACisyEy5eFfXE8t1KHcUzvA9I3CYpalBKJMnFDI41VYU0yHSc9WfGlZ1MedaSsP9y1AN5kmKI8BUqYVhRAsoY07B8NF1dFjCwt8cI-Ai-SIytakjWEg1iaFO1SfkyhSWp6K6ItMOdDF6VNDjVwAkjI17VxQT7RtMmBFNYVSCTUQwjEWOfelFzIBS1xDOIU3iG9Em3Q9WyjRIYMVJtSg80UkkbfTGKdV8TPxb8LQsgCrNo2zz3skDHMBBUYjjZVanhVx6lhYpfKkoyZLpXFQq4hB40jUpIlBDy1SggTwM1NDHynaSDHy0JpVKXimjiKJXFoqphKae9aoYx0zLAJq3lqaFN061Kk2VVLakSSDxVgxtoISfcclcVDkUGnpRp5I8T3VI7jtiZVBxaIA */
   createMachine(
     {
-      context: { blogs: [] },
-      tsTypes: {} as import('./blog-machine.typegen').Typegen0,
-      schema: {
-        context: {} as { blogs: BlogItem[] },
-        events: {} as
-          | {
-              type: 'done.invoke.blogMachine.FetchingBlogs.FetchingTistories.Fetching:invocation[0]';
-              data: BlogItem[];
-            }
-          | {
-              type: 'done.invoke.blogMachine.FetchingBlogs.FetchingMD.Fetching:invocation[0]';
-              data: BlogItem[];
-            }
-          | {
-              type: 'UPDATE';
-              data: BlogItem[];
-            }
-          | {
-              type: 'ERROR';
-            },
-      },
-      initial: 'FetchingBlogs',
-      id: 'blogMachine',
+  context: { blogs: [] },
+  tsTypes: {} as import('./blog-machine.typegen').Typegen0,
+  schema: {
+    context: {} as { blogs: BlogItem[] },
+    events: {} as
+      | {
+          type: 'done.invoke.blogMachine.FetchingBlogs.FetchingTistories.Fetching:invocation[0]';
+          data: BlogItem[];
+        }
+      | {
+          type: 'done.invoke.blogMachine.FetchingBlogs.FetchingMD.Fetching:invocation[0]';
+          data: BlogItem[];
+        }
+      | {
+          type: 'UPDATE';
+          data: BlogItem[];
+        }
+      | {
+          type: 'ERROR';
+        },
+  },
+  initial: 'FetchingBlogs',
+  id: 'blogMachine',
+  states: {
+    FetchingBlogs: {
+      type: 'parallel',
       states: {
-        FetchingBlogs: {
-          type: 'parallel',
+        FetchingTistories: {
+          initial: 'Fetching',
           states: {
-            FetchingTistories: {
-              initial: 'Fetching',
-              states: {
-                Done: {
-                  type: 'final',
-                },
-                Error: {
-                  type: 'final',
-                },
-                Fetching: {
-                  invoke: {
-                    src: 'fetchTistories',
-                    onDone: [
-                      {
-                        actions: 'updateBlogsContext',
-                        target: 'Done',
-                      },
-                    ],
-                    onError: [
-                      {
-                        target: 'Error',
-                      },
-                    ],
+            Done: {
+              type: 'final',
+            },
+            Error: {
+              type: 'final',
+            },
+            Fetching: {
+              invoke: {
+                src: 'fetchTistories',
+                onDone: [
+                  {
+                    actions: 'updateBlogsContext',
+                    target: 'Done',
                   },
-                },
+                ],
+                onError: [
+                  {
+                    target: 'Error',
+                  },
+                ],
               },
             },
-            FetchingMD: {
-              initial: 'Fetching',
-              states: {
-                Fetching: {
-                  invoke: {
-                    src: 'fetchMD',
-                    onDone: [
-                      {
-                        actions: 'updateBlogsContext',
-                        target: 'Done',
-                      },
-                    ],
-                    onError: [
-                      {
-                        target: 'Error',
-                      },
-                    ],
-                  },
-                },
-                Done: {
-                  type: 'final',
-                },
-                Error: {
-                  type: 'final',
-                },
-              },
-            },
-          },
-          onDone: {
-            target: 'Done',
           },
         },
-        Done: {},
+        FetchingMD: {
+          initial: 'Fetching',
+          states: {
+            Fetching: {
+              invoke: {
+                src: 'fetchMD',
+                onDone: [
+                  {
+                    actions: 'updateBlogsContext',
+                    target: 'Done',
+                  },
+                ],
+                onError: [
+                  {
+                    target: 'Error',
+                  },
+                ],
+              },
+            },
+            Done: {
+              type: 'final',
+            },
+            Error: {
+              type: 'final',
+            },
+          },
+        },
+      },
+      onDone: {
+        target: 'Done',
       },
     },
+    Done: {},
+  },
+},
     {
       actions: {
         updateBlogsContext: assign((ctx, event) => {
