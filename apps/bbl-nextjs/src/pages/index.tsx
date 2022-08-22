7932456e4c56655bc6e024120e77e4bdfe07f847
bobeenlee <globaldev@naver.com>
2022-08-22 13:55:43 +0900

    feat: 아바타 이미지 preload 적용

--- apps/bbl-nextjs/src/pages/index.tsx
+++ apps/bbl-nextjs/src/pages/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { HomeTemplate } from '@bbl-nx/ui-components';
 import Layout from '../components/layout';
-import Head from 'next/head';
-import Link from 'next/link';
-import { logoImages } from '@bbl-nx/images';
 
 const HomePage = () => {
   return (
     <Layout>
-      <Head>
-        <Link rel="preload" as="image" href={logoImages.quokkaPng} />
-      </Head>
       <HomeTemplate />
     </Layout>
   );
