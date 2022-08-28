db0b8203abaf230dd9c52249f2d0b57916d9e044
bobeenlee <globaldev@naver.com>
2022-08-28 15:37:08 +0900

    feat: update viewports stories if mobile

--- libs/ui-components/.storybook/preview.js
+++ libs/ui-components/.storybook/preview.js
@@ -2,6 +2,24 @@ import { ThemeProvider } from 'next-themes';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import tailwindCss from '!style-loader!css-loader!postcss-loader!./tailwind-imports.css';
 import { setImagePrefixPath } from '@bbl-nx/images';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const MY_VIEWPORTS = {
+  md: {
+    name: 'md-768px',
+    styles: {
+      width: '768px',
+      height: '1080px',
+    },
+  },
+  sm: {
+    name: 'sm-640px',
+    styles: {
+      width: '640px',
+      height: '1080px',
+    },
+  },
+};
 
 const STORYBOOK_PREFIX_PATH = process.env.STORYBOOK_PREFIX_PATH;
 if (STORYBOOK_PREFIX_PATH) {
@@ -19,6 +37,12 @@ export const parameters = {
       method: 'alphabetical',
       order: [],
       locales: '',
+    },
+  },
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      ...MY_VIEWPORTS,
     },
   },
 };
