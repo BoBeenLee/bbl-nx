8d380d4d7b88a0dc6702eff4db00979d3f9f58ac
bobeenlee <globaldev@naver.com>
2022-08-28 12:51:33 +0900

    chore: update prefix image path

--- libs/features/.storybook/preview.js
+++ libs/features/.storybook/preview.js
@@ -1,6 +1,13 @@
 import { ThemeProvider } from 'next-themes';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import tailwindCss from '!style-loader!css-loader!postcss-loader!./tailwind-imports.css';
+import { setImagePrefixPath } from '@bbl-nx/images';
+
+// github pages prefix path 설정
+const STORYBOOK_PREFIX_PATH = process.env.STORYBOOK_PREFIX_PATH;
+if (STORYBOOK_PREFIX_PATH) {
+  setImagePrefixPath(STORYBOOK_PREFIX_PATH);
+}
 
 const storybookStyles = document.createElement('style');
 storybookStyles.innerHTML = tailwindCss;
