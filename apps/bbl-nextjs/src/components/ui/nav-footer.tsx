134536cdb418d6af37fdfa3dcfcb5227aec00588
bobeenlee <globaldev@naver.com>
2023-03-05 23:13:15 +0900

    feat: 레이아웃 서버 컴포넌트 렌더로 변경

---
+++ apps/bbl-nextjs/src/components/ui/nav-footer.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from 'react'
+import UINavFooter from '@bbl-nx/ui-components/organisms/nav-footer/nav-footer'
+
+export default function NavFooter() {
+  return (
+    <UINavFooter />
+  )
+}
---
+++ apps/bbl-nextjs/src/components/ui/nav.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from 'react'
+import UINav from '@bbl-nx/ui-components/organisms/nav/nav'
+
+export default function Nav() {
+  return (
+    <UINav />
+  )
+}
