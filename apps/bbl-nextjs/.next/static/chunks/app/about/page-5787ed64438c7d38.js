6ca09adc1b2e0ea5d83e14fd9e7fb26cdd7deb70
이보빈[쇼핑라이브웹개발] <bobeen.lee@navercorp.com>
2023-03-13 10:08:19 +0900

    feat: .next 추가

---
+++ apps/bbl-nextjs/.next/static/chunks/app/about/page-5787ed64438c7d38.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,841,348],{2785:function(){}},function(n){n.O(0,[680,14,744],function(){return n(n.s=2785)}),_N_E=n.O()}]);
---
+++ apps/bbl-nextjs/.next/static/chunks/app/not-found-110056b4da75d48b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{3805:function(n,e,u){Promise.resolve().then(u.t.bind(u,5730,23))}},function(n){n.O(0,[730,680,14,744],function(){return n(n.s=3805)}),_N_E=n.O()}]);
